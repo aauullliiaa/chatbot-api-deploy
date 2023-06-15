3b8f3d08281378985c281be11e31ad8cc2c2f02b
Haris Daffa <79704418+harisde03@users.noreply.github.com>
2023-06-15 21:26:43 +0700

    feat: migrate api

--- main.py
+++ main.py
@@ -1,56 +1,18 @@
-# README
-# Hello everyone, in here I (Kaenova | Bangkit Mentor ML-20) 
-# will give you some headstart on createing ML API. 
-# Please read every lines and comments carefully. 
-# 
-# I give you a headstart on text based input and image based input API. 
-# To run this server, don't forget to install all the libraries in the
-# requirements.txt simply by "pip install -r requirements.txt" 
-# and then use "python main.py" to run it
-# 
-# For ML:
-# Please prepare your model either in .h5 or saved model format.
-# Put your model in the same folder as this main.py file.
-# You will load your model down the line into this code. 
-# There are 2 option I give you, either your model image based input 
-# or text based input. You need to finish functions "def predict_text" or "def predict_image"
-# 
-# For CC:
-# You can check the endpoint that ML being used, eiter it's /predict_text or 
-# /predict_image. For /predict_text you need a JSON {"text": "your text"},
-# and for /predict_image you need to send an multipart-form with a "uploaded_file" 
-# field. you can see this api documentation when running this server and go into /docs
-# I also prepared the Dockerfile so you can easily modify and create a container iamge
-# The default port is 8080, but you can inject PORT environement variable.
-# 
-# If you want to have consultation with me
-# just chat me through Discord (kaenova#2859) and arrange the consultation time
-#
-# Share your capstone application with me! 🥳
-# Instagram @kaenovama
-# Twitter @kaenovama
-# LinkedIn /in/kaenova
-
-## Start your code here! ##
-
 import os
 import uvicorn
 import traceback
 import tensorflow as tf
 
+import re
+import random
+import numpy as np
+
 from pydantic import BaseModel
-from urllib.request import Request
-from fastapi import FastAPI, Response, UploadFile
-from utils import load_image_into_numpy_array
+from fastapi import FastAPI, Response
+from keras.utils import pad_sequences
+from utils import tokenizer, lbl_enc, df, X
 
-# Initialize Model
-# If you already put yout model in the same folder as this main.py
-# You can load .h5 model or any model below this line
-
-# If you use h5 type uncomment line below
-# model = tf.keras.models.load_model('./my_model.h5')
-# If you use saved model type uncomment line below
-# model = tf.saved_model.load("./my_model_folder")
+model = tf.keras.models.load_model('./model.h5')
 
 app = FastAPI()
 
@@ -59,57 +21,40 @@ app = FastAPI()
 def index():
     return "Hello world from ML endpoint!"
 
-# If your model need text input use this endpoint!
 class RequestText(BaseModel):
     text:str
 
 @app.post("/predict_text")
 def predict_text(req: RequestText, response: Response):
     try:
-        # In here you will get text sent by the user
-        text = req.text
-        print("Uploaded text:", text)
+        req_text = req.text
+        print("Uploaded text:", req_text)
         
-        # Step 1: (Optional) Do your text preprocessing
+        # Step 1: Text preprocessing
+        text = []
+        txt = re.sub('[^a-zA-Z\']', ' ', req_text)
+        txt = txt.lower()
+        txt = txt.split()
+        txt = " ".join(txt)
+        text.append(txt)
         
-        # Step 2: Prepare your data to your model
-        
-        # Step 3: Predict the data
-        # result = model.predict(...)
-        
-        # Step 4: Change the result your determined API output
-        
-        return "Endpoint not implemented"
-    except Exception as e:
-        traceback.print_exc()
-        response.status_code = 500
-        return "Internal Server Error"
-
-# If your model need image input use this endpoint!
-@app.post("/predict_image")
-def predict_image(uploaded_file: UploadFile, response: Response):
-    try:
-        # Checking if it's an image
-        if uploaded_file.content_type not in ["image/jpeg", "image/png"]:
-            response.status_code = 400
-            return "File is Not an Image"
-        
-        # In here you will get a numpy array in "image" variable.
-        # You can use this file, to load and do processing
-        # later down the line
-        image = load_image_into_numpy_array(uploaded_file.file.read())
-        print("Image shape:", image.shape)
-        
-        # Step 1: (Optional, but you should have one) Do your image preprocessing
-        
-        # Step 2: Prepare your data to your model
+        # Step 2: Prepare data to model
+        x_test = tokenizer.texts_to_sequences(text)
+        x_test = np.ravel(x_test)
+        x_test = pad_sequences([x_test], padding='post', maxlen=X.shape[1])
         
         # Step 3: Predict the data
-        # result = model.predict(...)
+        y_pred = model.predict(x_test, verbose=0)
+        y_pred = y_pred.argmax()
+        tag = lbl_enc.inverse_transform([y_pred])[0]
         
-        # Step 4: Change the result your determined API output
-        
-        return "Endpoint not implemented"
+        # Step 4: Change the result of determined API output
+        responses = df[df['tag'] == tag]['responses'].values[0]
+        return {
+            "you" : req_text,
+            "bot" : random.choice(responses)
+        }
+    
     except Exception as e:
         traceback.print_exc()
         response.status_code = 500
@@ -117,7 +62,6 @@ def predict_image(uploaded_file: UploadFile, response: Response):
 
 
 # Starting the server
-# Your can check the API documentation easily using /docs after the server is running
 port = os.environ.get("PORT", 8080)
 print(f"Listening to http://0.0.0.0:{port}")
 uvicorn.run(app, host='0.0.0.0',port=port)
--- utils.py
+++ utils.py
@@ -1,7 +1,33 @@
-import numpy as np
+import json
+import pandas as pd
+from keras.preprocessing.text import Tokenizer
+from keras.utils import pad_sequences
+from sklearn.preprocessing import LabelEncoder
 
-from PIL import Image
-from io import BytesIO
+with open(r'./intents.json') as f:
+    data = json.load(f)
 
-def load_image_into_numpy_array(data):
-    return np.array(Image.open(BytesIO(data)))
+df = pd.DataFrame(data['intents'])
+
+dic = {"tag": [], "patterns": [], "responses": []}
+for i in range(len(df)):
+    ptrns = df[df.index == i]['patterns'].values[0]
+    rspns = df[df.index == i]['responses'].values[0]
+    tag = df[df.index == i]['tag'].values[0]
+    for j in range(len(ptrns)):
+        dic['tag'].append(tag)
+        dic['patterns'].append(ptrns[j])
+        dic['responses'].append(rspns)
+
+df = pd.DataFrame.from_dict(dic)
+
+tokenizer = Tokenizer(lower=True, split=' ')
+tokenizer.fit_on_texts(df['patterns'])
+tokenizer.get_config()
+
+vacab_size = len(tokenizer.word_index)
+ptrn2seq = tokenizer.texts_to_sequences(df['patterns'])
+X = pad_sequences(ptrn2seq, padding='post')
+
+lbl_enc = LabelEncoder()
+y = lbl_enc.fit_transform(df['tag'])
